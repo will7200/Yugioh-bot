cda35f395faf54aa992bdeaa793e3e8a8050032f
William Flores <w.f.08@live.com>
2017-10-06 13:31:12 -0700

    Formatted source

--- bot/trainer_matches.py
+++ bot/trainer_matches.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot as plt
 import PIL as Pillow
 from sklearn.cluster import KMeans
 
+
 class Trainer:
-    def __init__(self,query,x=0,y=0):
+    def __init__(self, query, x=0, y=0):
         self.query = query
         self.xThreshold = x
         self.yThreshold = y
@@ -14,97 +15,104 @@ class Trainer:
         elif type(query) is np.ndarray:
             self.query = query
         else:
-            self.query = cv2.imread(query,0)
+            self.query = cv2.imread(query, 0)
         self.goodMatches = []
         self.images = []
         self.circlePoints = []
         self.kmeans = None
         self.whitequery = None
-    def getMatches(self,train,corr):
-        img1 = cv2.imread(train,0) # trainImage
-        img2 = self.query # queryImage
+
+    def getMatches(self, train, corr):
+        img1 = cv2.imread(train, 0)  # trainImage
+        img2 = self.query  # queryImage
         # Initiate SIFT detector
         sift = cv2.xfeatures2d.SIFT_create()
 
         # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1,None)
-        kp2, des2 = sift.detectAndCompute(img2,None)
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
 
         # create BFMatcher object
         bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des1,des2, k=2)
+        matches = bf.knnMatch(des1, des2, k=2)
         goodMatches = []
         cluster = []
-        for m,n in matches:
+        for m, n in matches:
             img2_idx = m.trainIdx
             img1_idx = m.queryIdx
-            (x1,y1) = kp1[img1_idx].pt
-            (x2,y2) = kp2[img2_idx].pt
+            (x1, y1) = kp1[img1_idx].pt
+            (x2, y2) = kp2[img2_idx].pt
             #print("Comare %d to %d and %d to %d" % (x1,x2,y1,y2))
-            if m.distance < 0.8*n.distance and y2 > self.yThreshold and x2 < self.xThreshold:
+            if m.distance < 0.8 * n.distance and y2 > self.yThreshold and x2 < self.xThreshold:
                 goodMatches.append([m])
-                cluster.append([int(x2),int(y2)])
+                cluster.append([int(x2), int(y2)])
         if len(cluster) <= corr:
             return False
-        self.kmeans = KMeans(n_clusters=1, random_state = 0).fit(cluster)
-        #print(self.kmeans.cluster_centers_)
-        #plt.scatter(*zip(*cluster)),plt.axis([0,480,0,800]),plt.gca().invert_yaxis(),plt.show()
-        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,goodMatches,None,flags=2)
+        self.kmeans = KMeans(n_clusters=1, random_state=0).fit(cluster)
+        # print(self.kmeans.cluster_centers_)
+        # plt.scatter(*zip(*cluster)),plt.axis([0,480,0,800]),plt.gca().invert_yaxis(),plt.show()
+        img3 = cv2.drawMatchesKnn(
+            img1, kp1, img2, kp2, goodMatches, None, flags=2)
         self.images.append(img3)
-        #if train != 'assets/back__.png':
-        #plt.imshow(img3),plt.show()
+        # if train != 'assets/back__.png':
+        # plt.imshow(img3),plt.show()
         return True
-        #return goodMatches
+        # return goodMatches
         # cv2.drawMatchesKnn expects list of lists as matches.
-    def readCircles(self):
-        img = cv2.cvtColor(self.query,cv2.COLOR_BGR2GRAY)
-        img = cv2.medianBlur(img,7)
-        cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,30,
-                                    param1=50,param2=30,minRadius=20,maxRadius=50)
+    def readCircles(self):
+        img = cv2.cvtColor(self.query, cv2.COLOR_BGR2GRAY)
+        img = cv2.medianBlur(img, 7)
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 30,
+                                   param1=50, param2=30, minRadius=20, maxRadius=50)
         if circles is None:
             return
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
+        for i in circles[0, :]:
             # draw the outer circle
             if (i[1] < 400):
                 continue
-            self.circlePoints.append((i[0],i[1]))
-            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            self.circlePoints.append((i[0], i[1]))
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
-            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-        #plt.imshow(cimg),plt.show()
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+        # plt.imshow(cimg),plt.show()
         self.images.append(cimg)
+
     def whiteCircles(self):
         self.prepForWhiteCircles()
-        img = cv2.cvtColor(self.whitequery,cv2.COLOR_BGR2GRAY)
-        cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,40,
-                                    param1=50,param2=30,minRadius=5,maxRadius=60)
+        img = cv2.cvtColor(self.whitequery, cv2.COLOR_BGR2GRAY)
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 40,
+                                   param1=50, param2=30, minRadius=5, maxRadius=60)
         if circles is None:
             return
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            self.circlePoints.append((i[0],i[1]))
+        for i in circles[0, :]:
+            self.circlePoints.append((i[0], i[1]))
             # draw the outer circle
-            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
-            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
         #cv2.imshow("images", np.hstack([cimg]))
-        #cv2.waitKey(0)
+        # cv2.waitKey(0)
+
     def prepForWhiteCircles(self):
-        lower , upper = ([215,215,215], [255,255,255])
-        lower = np.array(lower, dtype = "uint8")
-        upper = np.array(upper, dtype = "uint8")
+        lower, upper = ([215, 215, 215], [255, 255, 255])
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
 
         # find the colors within the specified boundaries and apply
         # the mask
         mask = cv2.inRange(self.query, lower, upper)
-        output = cv2.bitwise_and(self.query, self.query, mask = mask)# coding=utf-8
+        output = cv2.bitwise_and(
+            self.query, self.query, mask=mask)  # coding=utf-8
         self.whitequery = output
+
     def compare(self):
         plot_image = self.images[0]
-        for x in range(1,len(self.images)):
-            plot_image = np.concatenate((plot_image,self.images[x]),axis=1)
-        plt.imshow(plot_image),plt.show()
+        for x in range(1, len(self.images)):
+            plot_image = np.concatenate((plot_image, self.images[x]), axis=1)
+        plt.imshow(plot_image), plt.show()
--- bot/utils.py
+++ bot/utils.py
@@ -18,8 +18,10 @@ import logging
 import inspect
 root = logging.getLogger('bot')
 
-#Rotate a python list
-def rotate_list(l,n):
+# Rotate a python list
+
+
+def rotate_list(l, n):
     return l[-n:] + l[:-n]
 
 
@@ -27,21 +29,26 @@ def Tap(x, y):
     curframe = inspect.currentframe()
     calframe = inspect.getouterframes(curframe, 2)
     #print('caller name:', calframe[1][3])
-    root.debug("Tapping at location ({},{})".format(x,y))
+    root.debug("Tapping at location ({},{})".format(x, y))
     Command = "bin\\adb.exe shell input tap %d %d" % (x, y)
     os.system(Command)
+
 
 def Swipe(x1, y1, x2, y2):
     Command = "bin\\adb.exe shell input swipe %d %d %d %d " % (x1, y1, x2, y2)
     os.system(Command)
 
+
 def SwipeTime(x1, y1, x2, y2, t):
-    Command = "bin\\adb.exe shell input swipe %d %d %d %d %d" % (x1, y1, x2, y2, t)
+    Command = "bin\\adb.exe shell input swipe %d %d %d %d %d" % (
+        x1, y1, x2, y2, t)
     os.system(Command)
+
 
 def KeyEscap():
     Command = "bin\\adb.exe shell input keyevent 4"
     os.system(Command)
+
 
 def TakePngScreenshot():
     while True:
@@ -49,17 +56,18 @@ def TakePngScreenshot():
             Command = "bin\\adb.exe shell \"screencap -p | busybox base64\""
             PngScreenshotData = os.popen(Command).read()
             PngScreenshotData = base64.b64decode(PngScreenshotData)
-            break;
+            break
         except KeyboardInterrupt:
             sys.exit(0)
         except:
-            print ("[!] Failed to get screen")
+            print("[!] Failed to get screen")
     return PngScreenshotData
 
+
 def IsColorInCeil(ColorToCheck, RefColor, Ceil):
-    if (RefColor[0]+(255*Ceil)) > ColorToCheck[0] > (RefColor[0]-(255*Ceil)):
-        if (RefColor[1]+(255*Ceil)) > ColorToCheck[1] > (RefColor[1]-(255*Ceil)):
-            if (RefColor[2]+(255*Ceil)) > ColorToCheck[2] > (RefColor[2]-(255*Ceil)):
+    if (RefColor[0] + (255 * Ceil)) > ColorToCheck[0] > (RefColor[0] - (255 * Ceil)):
+        if (RefColor[1] + (255 * Ceil)) > ColorToCheck[1] > (RefColor[1] - (255 * Ceil)):
+            if (RefColor[2] + (255 * Ceil)) > ColorToCheck[2] > (RefColor[2] - (255 * Ceil)):
                 return True
     return False
 
@@ -67,15 +75,16 @@ def IsColorInCeil(ColorToCheck, RefColor, Ceil):
 def GetMeanColor(img, x, y, size=10):
     MeanColor = [0, 0, 0]
     pixdata = img.load()
-    for xr in range(x-(size/2), x+(size/2)):
-        for yr in range(y-(size/2), y+(size/2)):
+    for xr in range(x - (size / 2), x + (size / 2)):
+        for yr in range(y - (size / 2), y + (size / 2)):
             MeanColor[0] = MeanColor[0] + pixdata[xr, yr][0]
             MeanColor[1] = MeanColor[1] + pixdata[xr, yr][1]
             MeanColor[2] = MeanColor[2] + pixdata[xr, yr][2]
-    MeanColor[0] = MeanColor[0]/(size**2)
-    MeanColor[1] = MeanColor[1]/(size**2)
-    MeanColor[2] = MeanColor[2]/(size**2)
+    MeanColor[0] = MeanColor[0] / (size**2)
+    MeanColor[1] = MeanColor[1] / (size**2)
+    MeanColor[2] = MeanColor[2] / (size**2)
     return MeanColor
+
 
 def GetImgFromScreenShot():
     Screenshot = TakePngScreenshot()
@@ -101,13 +110,13 @@ def ImgToString(img, CharSet=None):
     img.save("tmp\\ocr.png")
     Command = "bin\\tess\\tesseract.exe --tessdata-dir bin\\tess\\tessdata tmp\\ocr.png tmp\\ocr "
     if CharSet != None:
-        Command += "-c tessedit_char_whitelist="+CharSet+" "
+        Command += "-c tessedit_char_whitelist=" + CharSet + " "
     Command += "-psm 7 "
     Command += "> nul 2>&1"
-    #print Command
+    # print Command
     os.system(Command)
-    #TODO: Remove this, as we psm 7
-    #Get the largest line in txt
+    # TODO: Remove this, as we psm 7
+    # Get the largest line in txt
     with open("tmp\\ocr.txt") as f:
         content = f.read().splitlines()
     OutputLine = ""
@@ -117,7 +126,9 @@ def ImgToString(img, CharSet=None):
             OutputLine = line
     return OutputLine
 
-#Todo: Take a while...
+# Todo: Take a while...
+
+
 def ZoomOut():
     ZoomOutFix()
     Command = "bin\\adb.exe push bin\\Zoomout.txt /sdcard/Zoomout.txt"
@@ -125,7 +136,9 @@ def ZoomOut():
     Command = "bin\\adb.exe shell sh /sdcard/Zoomout.txt"
     os.system(Command)
 
-#Check the correct input
+# Check the correct input
+
+
 def GetEvent():
     Command = "bin\\adb.exe shell cat /proc/bus/input/devices >tmp\\inputs.log"
     os.system(Command)
@@ -137,13 +150,15 @@ def GetEvent():
         for line in content:
             line_num += 1
             if a in line:
-                x = line_num+4
+                x = line_num + 4
             elif line_num == x:
                 m = re.search("(event[0-9])", line)
                 result = m.group(1)
                 return result
 
-#Change the file whit the correct event number
+# Change the file whit the correct event number
+
+
 def ZoomOutFix():
     x = GetEvent()
     array = []
@@ -160,13 +175,16 @@ def ZoomOutFix():
     g.write("\n".join(array))
     g.close()
 
-#Check Nox process is running
+# Check Nox process is running
+
+
 def IsNoxRunning():
     try:
         if win32gui.FindWindow(None, "Nox"):
             return True
     except:
         return False
+
 
 def StartNoxProcess(NoxPath):
     try:
@@ -175,12 +193,14 @@ def StartNoxProcess(NoxPath):
     except:
         root.error("The program can't run Nox")
 
+
 def KillNoxProcess():
     try:
         if IsNoxRunning():
             os.system("taskkill /im Nox.exe /f")
     except:
         root.error("The program could not be killed")
+
 
 def LevenshteinDistance(first, second):
     """Find the Levenshtein distance between two strings."""
@@ -192,23 +212,25 @@ def LevenshteinDistance(first, second):
     second_length = len(second) + 1
     distance_matrix = [[0] * second_length for x in range(first_length)]
     for i in range(first_length):
-       distance_matrix[i][0] = i
+        distance_matrix[i][0] = i
     for j in range(second_length):
-       distance_matrix[0][j] = j
+        distance_matrix[0][j] = j
     for i in range(1, first_length):
         for j in range(1, second_length):
-            deletion = distance_matrix[i-1][j] + 1
-            insertion = distance_matrix[i][j-1] + 1
-            substitution = distance_matrix[i-1][j-1]
-            if first[i-1] != second[j-1]:
+            deletion = distance_matrix[i - 1][j] + 1
+            insertion = distance_matrix[i][j - 1] + 1
+            substitution = distance_matrix[i - 1][j - 1]
+            if first[i - 1] != second[j - 1]:
                 substitution += 1
             distance_matrix[i][j] = min(insertion, deletion, substitution)
-    return distance_matrix[first_length-1][second_length-1]
+    return distance_matrix[first_length - 1][second_length - 1]
+
 
 def WriteAppend(Filename, Content):
     f = open(Filename, "a")
     f.write(Content)
     f.close()
+
 
 def DiffImgPercent(img1, img2):
     pixdata1 = img1.load()
@@ -219,7 +241,8 @@ def DiffImgPercent(img1, img2):
             if pixdata1[xr, yr] != pixdata2[xr, yr]:
                 DiffCount += 1
 
-    return (DiffCount*1.0) / (img1.size[0]*img1.size[1])
+    return (DiffCount * 1.0) / (img1.size[0] * img1.size[1])
+
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
