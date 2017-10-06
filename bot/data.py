c532bbc1c7f3725da755582ebadb294fc1f2c979
William Flores <w.f.08@live.com>
2017-10-06 13:24:52 -0700

    Fixed format

--- bot/data.py
+++ bot/data.py
@@ -3,16 +3,18 @@ import datetime
 import bot.utils as utils
 
 data_object = {
-    'next_run_at':None,
-    'last_run_at':None,
-    'runnow':False,
-    'stop':False
+    'next_run_at': None,
+    'last_run_at': None,
+    'runnow': False,
+    'stop': False
 }
 data_object = utils.dotdict(data_object)
 data_file = "run_at.json"
 
+
 def setDatafile(filepath):
-    data_file=filepath
+    data_file = filepath
+
 
 def readDatefile(key=None):
     try:
@@ -27,20 +29,24 @@ def readDatefile(key=None):
         return data[key]
     return None
 
+
 def datetime_handler(x):
     if isinstance(x, datetime.datetime):
         return x.isoformat()
     raise TypeError("Unknown type")
 
+
 def date_hook(json_dict):
     for (key, value) in json_dict.items():
         try:
-            json_dict[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            json_dict[key] = datetime.datetime.strptime(
+                value, "%Y-%m-%dT%H:%M:%S.%f")
         except:
             pass
     return json_dict
 
+
 def writeDatefile(data):
     with open(data_file, 'w') as f:
         json.dump(data, f, sort_keys=True,
-        indent=4, separators=(',', ': '),default=datetime_handler)
+                  indent=4, separators=(',', ': '), default=datetime_handler)
