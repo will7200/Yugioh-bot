610e665869b96fdd5ac1cf686ceb5b1b87cf6111
William Flores <w.f.08@live.com>
2017-11-25 15:26:58 -0800

    Update command line, and move significant protion to duel links runtime

--- main.py
+++ main.py
@@ -1,176 +1,96 @@
+import signal
+import logging
+import logging.config
+
+import click
 import time
 import os
-import traceback
-import datetime
+
 import sys
-import threading
-import configparser
-import bot.utils as utils
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
-from watchdog.events import PatternMatchingEventHandler
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-from bot.data import set_data_file, write_data_file, read_data_file, data_object
-from bot.yugioh import tapnsleep, setup_logger, compare_with_back_button, DL_Bot
-from bot.shared import defaults_config, home_location, defaultlocations
+from apscheduler.schedulers.background import BackgroundScheduler
 
-#sys.stdout = open(os.path.dirname(os.path.realpath(__file__))+'\\file1.txt', 'a')
-Config = configparser.SafeConfigParser(defaults=defaults_config)
-Config.read("config.ini")
-root_dir = Config.get("locations", "home")
-if root_dir != home_location:
-    defaultlocations.newRoot(root_dir)
-    Config = configparser.SafeConfigParser(defaults=defaultlocations.getdict())
-    Config.read("config.ini")
-log_dir = Config.get("locations", "log")
-data_file = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "run_at.json")
-level = Config.get("logger", "level")
-root = setup_logger(log_dir, level, stream=Config.getboolean(
-    "logger", "stream"), includeap=Config.getboolean("logger", "includeapischeduler"))
-set_data_file(data_file)
-sched = BlockingScheduler()
-
-eventLock = threading.Lock()
+import yaml
+from bot import logger
+from bot.providers import get_provider
 
 
-class WatchFile(PatternMatchingEventHandler):
-    runnable = True
-    thread = None
+def setup_logging(
+        default_path='logging.yaml',
+        default_level=logging.INFO,
+        env_key='LOG_CFG'
+):
+    """Setup logging configuration
 
-    def on_moved(self, event):
-        super(WatchFile, self).on_moved(event)
-        root.debug("File %s was just moved" % event.src_path)
-
-    def on_created(self, event):
-        super(WatchFile, self).on_created(event)
-        root.debug("File %s was just created" % event.src_path)
-
-    def on_deleted(self, event):
-        super(WatchFile, self).on_deleted(event)
-        root.debug("File %s was just deleted" % event.src_path)
-
-    def on_modified(self, event):
-        super(WatchFile, self).on_modified(event)
-        root.debug("File %s was just modified" % event.src_path)
-        if self.runnable:
-            self.lock()
-            self.check_runat()
-            self.check_stop()
-            self.scheduleunlock()
-
-    def lock(self):
-        eventLock.acquire()
-        self.runnable = False
-        eventLock.release()
-
-    def unlock(self):
-        eventLock.acquire()
-        self.runnable = True
-        eventLock.release()
-
-    def scheduleunlock(self):
-        when = datetime.datetime.now() + datetime.timedelta(seconds=4)
-        sched.add_job(self.unlock, trigger='date', id='unlock_file at %s' % (
-            when.isoformat()), run_date=when)
-
-    def check_runat(self):
-        next_run_at = read_data_file()
-        if 'runnow' in next_run_at and next_run_at['runnow'] is True:
-            root.debug("Forcing run now")
-            if self.thread is None:
-                sched.remove_all_jobs()
-                sched.add_job(main, id='cron_main_force')
-            else:
-                root.debug("Thread is currently running")
-            next_run_at['runnow'] = False
-            write_data_file(next_run_at)
-
-    def check_stop(self):
-        data = read_data_file()
-        if self.thread is None:
-            data.stop = False
-            write_data_file(data)
-            return
-        if 'stop' in data and data['stop'] is True:
-            for x in threading.enumerate():
-                if x == self.thread:
-                    x.do_run = False
-            root.debug("Emitting stop event")
-            data.stop = False
-            write_data_file(data)
-        elif 'stop' in data and data['stop'] is False:
-            root.debug("Emitting resume event")
-
-    def current_thread(self, thread):
-        self.thread = thread
-
-
-event_handler = WatchFile(patterns=[data_file])
-
-
-def main():
-    data = read_data_file()
-    data.last_run_at = datetime.datetime.now()
-    write_data_file(data)
-    try:
-        if not utils.IsNoxRunning():
-            utils.StartNoxProcess(os.path.join(
-                Config.get('bot', 'noxlocation'), 'Nox.exe'))
-            time.sleep(30)
-            tapnsleep((25, 550), 10)
-            tapnsleep((240, 540), 45)
-        compare_with_back_button()
-        bot = DL_Bot(sched)
-        bot.auto(event_handler.current_thread)
-        if Config.getboolean("bot", "killnoxondone"):
-            utils.KillNoxProcess()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-        print(traceback.format_exc())
-    data.next_run_at = datetime.datetime.now() + datetime.timedelta(hours=4)
-    d = data.next_run_at
-    write_data_file(data)
-    sched.add_job(main, trigger='date', id='cron_main_at_%s' %
-                  (d.isoformat()), run_date=data.next_run_at)
-
-
-def start():
-    sched.start()
-
-
-executors = {
-    'default': {'type': 'threadpool', 'max_workers': 20},
-    'processpool': ProcessPoolExecutor(max_workers=5)
-}
-sched.configure(executors=executors)
-if __name__ == "__main__":
-    t = threading.Thread(target=start, args=())
-    t.start()
-    next_run_at = read_data_file('next_run_at')
-    if Config.getboolean("bot", "startonstartup"):
-        next_run_at = datetime.datetime.now() + datetime.timedelta(seconds=1)
-    elif next_run_at == None:
-        next_run_at = datetime.datetime.now() + datetime.timedelta(seconds=5)
-    elif datetime.datetime.now() > next_run_at:
-        next_run_at = datetime.datetime.now() + datetime.timedelta(seconds=5)
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
     else:
-        nextAt = next_run_at - datetime.datetime.now()
-        next_run_at = datetime.datetime.now(
-        ) + datetime.timedelta(seconds=nextAt.total_seconds())
-    sched.add_job(main, trigger='date', id='cron_main_at_%s' %
-                  (next_run_at.isoformat()), run_date=next_run_at)
-    root.info("Tracking %s" % (data_file))
-    root.info('Next run at %s' % (next_run_at.isoformat()))
-    observer = Observer()
-    observer.schedule(event_handler, os.path.dirname(
-        os.path.realpath(__file__)), recursive=True)
-    observer.start()
-    t.join()
-    #r = input("Get Some Input")
-    sched.shutdown()
-    observer.stop()
-    sys.exit(0)
+        logging.basicConfig(level=default_level)
+
+
+setup_logging()
+
+from bot.utils.common import make_config_file, default_config
+from bot.duel_links_runtime import DuelLinkRunTime
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option("--generate-config", default=False, help="Generate Config file", is_flag=True)
+@click.option("--file-path", default="configtest.ini", help="File Location")
+def config(generate_config, file_path):
+    if generate_config:
+        make_config_file(file_path)
+
+
+@click.command()
+@click.option("-s", "--start", is_flag=True, default=False)
+@click.option("-c", "--config-file", default="config.ini")
+def bot(start, config_file):
+    if start:
+        """
+        Due to: forrtl: error (200): program aborting due to control-C event
+        I need to intercept control-c signal to correctly close down this program
+        """
+
+        def handler(signum, frame):
+            if signum == signal.SIGINT:
+                dlRuntime.shutdown()
+                scheduler.shutdown()
+                logger.info("Exiting")
+                sys.exit(0)
+
+        signal.signal(signal.SIGINT, handler)
+        # TODO run bot
+        uconfig = default_config()
+        uconfig.read(config_file)
+        scheduler = BackgroundScheduler()
+        dlRuntime = DuelLinkRunTime(uconfig, scheduler)
+        scheduler.start()
+        dlRuntime.set_provider(get_provider(uconfig.get('bot', 'provider'))(scheduler, uconfig))
+        dlRuntime.main()
+
+        while True:
+            try:
+                time.sleep(5)
+                if dlRuntime._shutdown:
+                    return
+            except InterruptedError:
+                pass
+
+
+cli.add_command(bot)
+cli.add_command(config)
+
+if __name__ == "__main__":
+    cli()
