770a0648dd2b4f0c87bcf7f56d23b29c3ffeb3ef
William Flores <w.f.08@live.com>
2017-11-24 22:28:23 -0800

    Update debug helpers

--- bot/debug_helpers/helpers_decorators.py
+++ bot/debug_helpers/helpers_decorators.py
@@ -16,3 +16,22 @@ def calling_function(__function):
         return __function(*args, **kwargs)
 
     return wrapper
+
+def async_calling_function(callFrame):
+    def calling_functionn(__function):
+        """
+        View Calling Function to debug
+        """
+
+        def wrapper(*args, **kwargs):
+            cur_frame = inspect.currentframe()
+            cal_frame = inspect.getouterframes(cur_frame, callFrame)
+            stack = []
+            for t in cal_frame:
+                if 'Yu-gi' in t[1]:
+                    stack.append(t[3])
+            logger.debug("Called by: " + str(stack))
+            return __function(*args, **kwargs)
+
+        return wrapper
+    return calling_functionn
--- bot/debug_helpers/logger.py
+++ bot/debug_helpers/logger.py
@@ -1,13 +1,19 @@
 import logging
+from collections import deque
 
 
 class LastRecordHandler(logging.Handler):
-    def __init__(self):
+    def __init__(self, max_length=10):
         logging.Handler.__init__(self)
-        self.lastRecord = None
+        self.lastRecords = deque(maxlen=max_length)
+        self.max_length = max_length
 
     def emit(self, record):
-        self.lastRecord = record
+        self.lastRecords.append(record)
 
-    def get_record(self):
-        return self.lastRecord.getMessage()
+    def get_record(self, index=-1):
+        " Defaults to getting the last know record"
+        if len(self.lastRecords) > 0 and -1 <= index < min(self.max_length,len(self.lastRecords)):
+            return self.lastRecords[index]
+        else:
+            return None
