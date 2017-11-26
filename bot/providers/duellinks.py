c20d4a344ecc630222ccb1a63c8d04b34909c7b3
William Flores <w.f.08@live.com>
2017-11-25 16:29:05 -0800

    Classed Predefined Values for each provider

--- bot/providers/duellinks.py
+++ bot/providers/duellinks.py
@@ -1,6 +1,11 @@
 from abc import abstractmethod
 
 import time
+from datetime import datetime
+import numpy as np
+import cv2
+import h5py
+import os
 
 from .shared import *
 
@@ -86,6 +91,92 @@ class EventExecutor(object):
             func(**_event.args)
             return True
         return False
+
+
+class Predefined(object):
+    _config = None
+    dataset = None
+    version = None
+    assets = None
+
+    def __init__(self, config, version):
+        self._config = config
+        self.cache_file = config.get('locations', 'cache_file')
+        self.dataset = self.__class__.__name__
+        self.assets = config.get('locations','assets')
+        self.version = version
+
+    _cache = None
+    _last_read = datetime.fromtimestamp(0)
+
+    @property
+    def cache(self):
+        self.get_cache()
+        return self._cache
+
+    @cache.setter
+    def cache(self, value):
+        self._last_read = datetime.now()
+        self._cache = value
+
+    def get_cache(self):
+        if not os.path.exists(self.cache_file):
+            self.generate()
+        if self.cache is None:
+            self.cache = h5py.File(self.cache_file)
+            if self.dataset in self.cache.keys():
+                df = self.cache.get(self.dataset)
+                version = df.get('version', 0)
+                if version == 0 or version != self.version:
+                    self.generate()
+                    self.cache = h5py.File(self.cache_file)
+
+    _duel_varient = None
+
+    @property
+    def duel_variant(self):
+        raise NotImplementedError("Class {} did not implement duel variant property".format(self.__class__.__name__))
+
+    _auto_duel = None
+
+    @property
+    def autoduel(self):
+        raise NotImplementedError("Class {} did not implement auto duel property".format(self.__class__.__name__))
+
+    # TODO: IMPLEMENT METHOD TO DETERMINE THE ACCURACY OR THE LIKELHOOD THAT THIS IS AN AUTODUEL BUTTON
+
+
+    def determine_autoduel_status(self, img):
+        vals = self.cache
+        autodueloff = vals['auto_duel_off']
+        autoduelon = vals['auto_duel_on']
+        current = self.get_image_stats(img, **self.autoduel)
+        dist1 = np.linalg.norm(current - autoduelon)
+        dist2 = np.linalg.norm(current - autodueloff)
+        if dist1 < dist2:
+            return True
+        return False
+
+    def determine_duel_variant(self, img):
+        vals = self.cache
+        ver_duel_variant = vals['duel_variant']
+        edges = cv2.Canny(img, 240, 255)
+        current = Predefined.get_image_stats(edges, **self.duel_variant)
+        dist1 = np.linalg.norm(current - ver_duel_variant)
+        if dist1 <= 5:
+            return True
+        return False
+
+    @staticmethod
+    def get_image_stats(img, left=0, top=0, width=0, height=0):
+        crop_img = img[top:(top + height), left:(left + width)]
+        (means, stds) = cv2.meanStdDev(crop_img)
+        stats = np.concatenate([means, stds]).flatten()
+        return stats
+
+    @abstractmethod
+    def generate(self):
+        raise NotImplementedError("Class {} did not implement generate".format(self.__class__.__name__))
 
 
 class DuelLinks(object):
