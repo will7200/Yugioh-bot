9a0f90bbd687b32fcb7f24c00af469996c41dc65
William Flores <w.f.08@live.com>
2018-01-27 18:54:18 -0800

    chore: moved files to base folder

--- bot/common.py
+++ bot/common.py
@@ -63,3 +63,17 @@ def crop_image(img, left=0, top=0, width=0, height=0):
 def bot_assertion(condition, error_type, message):
     if not condition:
         raise error_type(message)
+
+
+def first_true(iterable, default=False, pred=None):
+    """Returns the first true value in the iterable.
+
+    If no true value is found, returns *default*
+
+    If *pred* is not None, returns the first item
+    for which pred(item) is true.
+
+    """
+    # first_true([a,b,c], x) --> a or b or c or x
+    # first_true([a,b], x, f) --> a if f(a) else b if f(b) else x
+    return next(filter(pred, iterable), default)
