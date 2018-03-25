38ff3493b9abc34ef1548d969ef6cad81b58ee67
William Flores <w.f.08@live.com>
2018-03-24 20:16:57 -0700

    build: update post-script

--- fix_build.py
+++ fix_build.py
@@ -1,13 +1,16 @@
 import os
 from distutils.dir_util import copy_tree
+from glob import glob
 
 
 def copy_file():
-    print(os.listdir(os.path.join(os.environ.get('PYTHON'),'Lib')))
-    print(os.listdir(os.environ.get('PYTHON')))
-    copy_tree(os.path.join(os.environ.get('PYTHON'), r'Lib\site-packages\APScheduler-3.4.0-py3.5.egg-info'), 'dist/dlbot/APScheduler-3.4.0-py3.5.egg-info')
-    copy_tree(r'dist/dlbot/PyQt5/Qt/plugins',r'dist/dlbot')
-    os.remove('dist/dlbot/APScheduler-3.4.0-py3.5.egg-info/requires.txt')
+    site_packages = os.path.join(os.environ.get('PYTHON'), 'Lib', 'site-packages')
+    apscheduler_egg = glob(os.path.join(site_packages, 'APScheduler-*'))[0]
+    apscheduler_foldername = os.path.basename(apscheduler_egg)
+    copy_tree(apscheduler_egg,
+              'dist/dlbot/{}'.format(apscheduler_foldername))
+    copy_tree(r'dist/dlbot/PyQt5/Qt/plugins', r'dist/dlbot')
+    os.remove('dist/dlbot/{}/requires.txt'.format(apscheduler_foldername))
 
 
 if __name__ == "__main__":
