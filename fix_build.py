335dae58fdb41eedd143167a9dcbe629a4c9a191
William Flores <w.f.08@live.com>
2018-03-24 20:06:09 -0700

    build: update post-script

--- fix_build.py
+++ fix_build.py
@@ -3,6 +3,8 @@ from distutils.dir_util import copy_tree
 
 
 def copy_file():
+    print(os.listdir(os.path.join(os.environ.get('PYTHON'),'Lib')))
+    print(os.listdir(os.environ.get('PYTHON')))
     copy_tree(os.path.join(os.environ.get('PYTHON'), r'Lib\site-packages\APScheduler-3.4.0-py3.5.egg-info'), 'dist/dlbot/APScheduler-3.4.0-py3.5.egg-info')
     copy_tree(r'dist/dlbot/PyQt5/Qt/plugins',r'dist/dlbot')
     os.remove('dist/dlbot/APScheduler-3.4.0-py3.5.egg-info/requires.txt')
