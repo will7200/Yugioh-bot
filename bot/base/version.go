fb500c392b9771789f59369e68c7a1d5d7e25d3e
William Flores <w.f.08@live.com>
2018-04-13 21:25:42 -0700

    update: removed copyright messages

--- bot/base/version.go
+++ bot/base/version.go
@@ -3,4 +3,8 @@ package base
 
 var Version string
 var BuildDate string
-var CommitHash string
+var GitCommit string
+var GitSummary string
+var GitState string
+var GitBranch string
+
--- bot/cmd/version.go
+++ bot/cmd/version.go
@@ -1,23 +1,8 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
 	"github.com/spf13/cobra"
 	"runtime"
-	"strings"
 	"github.com/will7200/Yugioh-bot/bot/base"
 	"fmt"
 )
@@ -37,9 +22,9 @@ func init() {
 }
 
 func printVersion() {
-	if base.CommitHash == "" {
+	if base.GitCommit == "" {
 		fmt.Printf("Yu-gi-oh Duel Links Bot v%s %s/%s Build Date: %s\n", base.Version, runtime.GOOS, runtime.GOARCH, base.BuildDate)
 	} else {
-		fmt.Printf("Yu-gi-oh Duel Links Bot v%s-%s %s/%s Build Date: %s\n", base.Version, strings.ToUpper(base.CommitHash), runtime.GOOS, runtime.GOARCH, base.BuildDate)
+		fmt.Printf("Yu-gi-oh Duel Links Bot v%s-%s %s/%s Build Date: %s\n", base.Version, base.GitSummary, runtime.GOOS, runtime.GOARCH, base.BuildDate)
 	}
 }
