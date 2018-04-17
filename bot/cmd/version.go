f9fbb112d8eacb1e8732f20f517293aec5d75500
William Flores <w.f.08@live.com>
2018-04-16 22:41:52 -0700

    fix: Update version string formatting

--- bot/cmd/version.go
+++ bot/cmd/version.go
@@ -10,8 +10,8 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Prints the version of the bot -- Yu-gi-oh Duel Links",
-	Long: `Keep Track of that version bot.`,
+	Short: "Displays the version of the bot",
+	Long:  `Keep Track of that version bot.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		printVersion()
 	},
@@ -25,6 +25,6 @@ func printVersion() {
 	if base.GitCommit == "" {
 		fmt.Printf("Yu-gi-oh Duel Links Bot v%s %s/%s Build Date: %s\n", base.Version, runtime.GOOS, runtime.GOARCH, base.BuildDate)
 	} else {
-		fmt.Printf("Yu-gi-oh Duel Links Bot v%s-%s %s/%s Build Date: %s\n", base.Version, base.GitSummary, runtime.GOOS, runtime.GOARCH, base.BuildDate)
+		fmt.Printf("Yu-gi-oh Duel Links Bot %s %s/%s Build Date: %s\n", base.GitSummary, runtime.GOOS, runtime.GOARCH, base.BuildDate)
 	}
 }
