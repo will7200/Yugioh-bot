0125f23fe5bc05eb0b375b5ba477965fb59b2769
William Flores <w.f.08@live.com>
2018-04-25 22:48:23 -0700

    feat(nox): add remote capabilities
    Nox provider allows for connections to remote adb server instances, as well as device selection
    to operate solely on

--- bot/cmd/run_nox.go
+++ bot/cmd/run_nox.go
@@ -1,59 +1,40 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/prometheus/common/log"
-	"github.com/sirupsen/logrus"
-	"os/signal"
+	"errors"
 	"os"
-	"github.com/will7200/Yugioh-bot/bot/base"
-	"fmt"
+	"os/signal"
 	"time"
-	"os/exec"
+
+	"github.com/ahmetb/go-linq"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/will7200/Yugioh-bot/bot"
+	"github.com/will7200/Yugioh-bot/bot/base"
+	"github.com/will7200/Yugioh-bot/bot/providers"
+	_ "github.com/will7200/Yugioh-bot/bot/providers/nox"
+	nox2 "github.com/will7200/Yugioh-bot/bot/providers/nox"
 	"github.com/zach-klippenstein/goadb"
-	"io/ioutil"
 )
 
-var (
-	client *adb.Adb
+const (
+	noxAdbHost     = "nox.adb-host"
+	noxAdbPort     = "nox.adb-port"
+	noxAdbLocation = "nox.adb-location"
+	noxAdbDevice   = "nox.adb-device"
+	noxAdbRemote   = "nox.adb-remote"
+	noxAdbTimeout  = "nox.adb-timeout"
+	noxPath        = "nox.path"
 )
+
 // botCmd represents the bot command
 var botCmd = &cobra.Command{
 	Use:   "nox",
-	Short: "Starts the bot using a nox provider",
-	Long:  `Starts the bot using nox as the emulator for Duel Links Bot`,
-	Run: func(cmd *cobra.Command, args []string) {
-		l := log.NewPassedLogger(logrus.StandardLogger())
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		d := &base.Dispatcher{}
-		d.StartDispatcher(4)
-		j := &base.Job{Name: "Test", Epsilon: "PT5S", ID: "1", IsActive: true}
-		j.Schedule = fmt.Sprintf("R/%s/PT2S", time.Now().Add(2 * time.Second).Format("2006-01-02T15:04:05Z-0700"))
-		err := j.ParseSchedule()
-		if err != nil {
-			l.Errorln(err)
-		}
-		j.StartWaiting(d)
-		newPath := os.Getenv("PATH")
-		newPath += ":/Applications/Nox App Player.app/Contents/MacOS"
-		os.Setenv("PATH", newPath)
-		cmd1 := exec.Command("adb", "devices")
-		cmd1.Stdout = os.Stdout
-		cmd1.Stderr = os.Stderr
-		cmd1.Env = append(os.Environ(),
-			"FOO=duplicate_value", // ignored
-			"FOO=actual_value",    // this value is used
-		)
-		//if err := cmd1.Run(); err != nil {
-		//	l.Fatal(err)
-		//}
-		go t()
-		//fmt.Println(binary)
-		<-c
-		l.Infoln("Exiting Now")
-		os.Exit(1)
-	},
+	Short: "Start a bot instance using a nox instance",
+	Long: `Starts the bot using nox as the emulator for Duel Links Bot. 
+This is not limited to one instance, as long as the device is not in use by another bot instance.
+Note if you are connecting to a remote adb instance, the local client and server must match versions.`,
+	Run: noxInstance,
 }
 
 func init() {
@@ -61,131 +42,72 @@ func init() {
 
 	botCmd.Flags().String("adb-host", "", "adb server host name (defaults to localhost)")
 	botCmd.Flags().Int("adb-port", 0, "adb server port (defaults to 5037)")
-	botCmd.Flags().String("adb-path", "", "adb")
+	botCmd.Flags().String("adb-location", "", "adb location on filesystem, include adb as well")
+	botCmd.Flags().String("adb-device", "", "adb device name (cab be ip with port as well)")
+	botCmd.Flags().Bool("adb-remote", false, "using a remote adb server so it nows how to start it if applicable")
+	botCmd.Flags().Int("adb-timeout", 10, "timeout for adb to dial correctly")
+	viper.BindPFlag(noxAdbHost, botCmd.Flag("adb-host"))
+	viper.BindPFlag(noxAdbPort, botCmd.Flag("adb-port"))
+	viper.BindPFlag(noxAdbLocation, botCmd.Flag("adb-location"))
+	viper.BindPFlag(noxAdbDevice, botCmd.Flag("adb-device"))
+	viper.BindPFlag(noxAdbRemote, botCmd.Flag("adb-remote"))
+	viper.BindPFlag(noxAdbTimeout, botCmd.Flag("adb-timeout"))
 }
 
-func t() {
-	var err error
-	client, err = adb.NewWithConfig(adb.ServerConfig{PathToAdb: "/Users/williamflores/Downloads/platform-tools/adb", Host: "192.168.1.14"})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Starting server…")
-	//client.StartServer()
+type remoteStart struct {
+}
 
-	serverVersion, err := client.ServerVersion()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Server version:", serverVersion)
+func (t *remoteStart) Start() error {
+	return errors.New("Remote Starting adb is not supported yet")
+}
 
+func noxInstance(cmd *cobra.Command, args []string) {
+	d := &base.Dispatcher{}
+	d.StartDispatcher(viper.GetInt(botWorkers))
+	rs := remoteStart{}
+	client, err := adb.NewWithConfig(adb.ServerConfig{
+		PathToAdb:    viper.GetString(noxAdbLocation),
+		Host:         viper.GetString(noxAdbHost),
+		Port:         viper.GetInt(noxAdbPort),
+		Dialer:       adb.TcpDialerTimeout{Timeout: time.Second * time.Duration(viper.GetInt(noxAdbTimeout))},
+		Remote:       viper.GetBool(noxAdbRemote),
+		RemoteServer: adb.RemoteServer(&rs),
+	})
+	if err != nil {
+		log.Error("Could not connect to the adb server")
+		log.Fatal("Try again")
+	}
 	devices, err := client.ListDevices()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Devices:")
-	for _, device := range devices {
-		fmt.Printf("\t%+v\n", *device)
-	}
-
-	serials, err := client.ListDeviceSerials()
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, serial := range serials {
-		PrintDeviceInfoAndError(adb.DeviceWithSerial(serial))
-	}
-
-	fmt.Println()
-	fmt.Println("Watching for device state changes.")
-	watcher := client.NewDeviceWatcher()
-	for event := range watcher.C() {
-		fmt.Printf("\t[%s]%+v\n", time.Now(), event)
-	}
-	if watcher.Err() != nil {
-		printErr(watcher.Err())
-	}
-}
-
-func printErr(err error) {
-	switch err := err.(type) {
-
-	default:
-		fmt.Println("error:", err)
-	}
-}
-
-func PrintDeviceInfoAndError(descriptor adb.DeviceDescriptor) {
-	device := client.Device(descriptor)
-	if err := PrintDeviceInfo(device); err != nil {
-		log.Error(err)
-	}
-}
-
-func PrintDeviceInfo(device *adb.Device) error {
-	serialNo, err := device.Serial()
-	if err != nil {
-		return err
-	}
-	devPath, err := device.DevicePath()
-	if err != nil {
-		return err
-	}
-	state, err := device.State()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(device)
-	fmt.Printf("\tserial no: %s\n", serialNo)
-	fmt.Printf("\tdevPath: %s\n", devPath)
-	fmt.Printf("\tstate: %s\n", state)
-
-	cmdOutput, err := device.RunCommand("pwd")
-	if err != nil {
-		fmt.Println("\terror running command:", err)
-	}
-	fmt.Printf("\tcmd output: %s\n", cmdOutput)
-
-	stat, err := device.Stat("/sdcard")
-	if err != nil {
-		fmt.Println("\terror stating /sdcard:", err)
-	}
-	fmt.Printf("\tstat \"/sdcard\": %+v\n", stat)
-
-	fmt.Println("\tfiles in \"/\":")
-	entries, err := device.ListDirEntries("/")
-	if err != nil {
-		fmt.Println("\terror listing files:", err)
+	var device *adb.DeviceInfo
+	if len(devices) == 1 {
+		device = devices[0]
 	} else {
-		for entries.Next() {
-			fmt.Printf("\t%+v\n", *entries.Entry())
-		}
-		if entries.Err() != nil {
-			fmt.Println("\terror listing files:", err)
-		}
+		var serialDevices []string
+		linq.From(devices).Select(func(c interface{}) interface{} {
+			return c.(*adb.DeviceInfo).Serial
+		}).ToSlice(&serialDevices)
+		log.Error("Need to choose a device since there is more than one connected")
+		log.Errorf("Choose from %+v", serialDevices)
+		log.Fatal("Re-run command with --adb-device={{ device name }}")
 	}
-
-	fmt.Println("\tnon-existent file:")
-	stat, err = device.Stat("/supercalifragilisticexpialidocious")
-	if err != nil {
-		fmt.Println("\terror:", err)
-	} else {
-		fmt.Printf("\tstat: %+v\n", stat)
+	log.Infof("Using device %s", device.Serial)
+	options := &providers.Options{
+		Dispatcher:  d,
+		IsRemote:    viper.GetBool(noxAdbRemote),
+		SleepFactor: viper.GetInt(botSleepFactor),
+		Path:        viper.GetString(noxPath),
 	}
-
-	fmt.Print("\tload avg: ")
-	loadavgReader, err := device.OpenRead("/proc/loadavg")
-	if err != nil {
-		fmt.Println("\terror opening file:", err)
-	} else {
-		loadAvg, err := ioutil.ReadAll(loadavgReader)
-		if err != nil {
-			fmt.Println("\terror reading file:", err)
-		} else {
-			fmt.Println(string(loadAvg))
-		}
-	}
-
-	return nil
+	nox := providers.GetProvider("Nox", options)
+	options.Provider = nox
+	noxp := interface{}(nox).(*nox2.NoxProvider)
+	noxp.SetClientDevice(client, adb.DeviceWithSerial(device.Serial))
+	runTime := bot.NewRunTime(d, nox)
+	runTime.Start()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	log.Infoln("Exiting Now")
 }
