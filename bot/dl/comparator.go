093e9faf14316703f4099a2619c0e692e6d639e6
William Flores <will7207@gmail.com>
2018-05-21 22:50:39 -0700

    feat(dl): add comparator
    Comparator is an interface that compares two given images, either by default opencv's SIFT
    algorithm or by other means.

---
+++ bot/dl/comparator.go
@@ -0,0 +1,99 @@
+package dl
+
+import (
+	"fmt"
+
+	cluster2 "github.com/cdipaolo/goml/cluster"
+	"github.com/patrickmn/go-cache"
+	"github.com/will7200/Yugioh-bot/bot/base"
+	"gocv.io/x/gocv"
+	"gocv.io/x/gocv/contrib"
+)
+
+// Correlation
+type Correlation int
+
+// comparator
+type comparator struct {
+	predefined *Predefined
+	cache      *cache.Cache
+	options    *Options
+}
+
+// Compare
+func (c *comparator) Compare(key string, img gocv.Mat, correlation Correlation) bool {
+	asset := c.predefined.GetAsset(AssetPrefix + key)
+	if asset == (AssetMap{}) {
+		log.Panic(fmt.Sprintf("Comparator %s does not exist", key))
+	}
+	trainImage, err := TryImageFromCache(asset, *c.options, c.cache)
+	if err != nil {
+		log.Panic(err)
+	}
+	var scaleFactor float64
+	scaleFactor = asset.ScaleFactor
+	if asset.ScaleFactor == 0 {
+		scaleFactor = .80
+	}
+	sift := contrib.NewSIFT()
+	defer sift.Close()
+	mask := gocv.NewMat()
+
+	kp1, des1 := sift.DetectAndCompute(trainImage, mask)
+	kp2, des2 := sift.DetectAndCompute(img, mask)
+	defer des1.Close()
+	defer des2.Close()
+
+	if des1.Empty() || des2.Empty() {
+		return false
+	}
+	bf := gocv.NewBFMatcher()
+	defer bf.Close()
+	matches := bf.KnnMatch(des1, des2, 2)
+	goodMatches := make([]gocv.DMatch, 0, len(matches))
+	cluster := make([][]float64, 0, len(matches))
+	for i := range matches {
+		m, n := matches[i][0], matches[i][1]
+		kpQuery := kp1[m.QueryIdx]
+		kpTrain := kp2[m.TrainIdx]
+		_, p2 := base.PointFromKeyPoint(kpQuery), base.PointFromKeyPoint(kpTrain)
+		if m.Distance < scaleFactor*n.Distance && p2.Y > asset.YThres && p2.X < asset.XThres {
+			goodMatches = append(goodMatches, m)
+			cluster = append(cluster, []float64{kpTrain.X, kpTrain.Y})
+		}
+	}
+	if len(cluster) < int(correlation) {
+		return false
+	}
+	model := cluster2.NewKMeans(1, 300, cluster)
+
+	if model.Learn() != nil {
+		log.Panic("Could not train model")
+	}
+	// log.Info(model.Centroids[0])
+	// matches =
+	return false
+}
+
+// Comparator
+type Comparator interface {
+	Compare(key string, img gocv.Mat, correlation Correlation) bool
+}
+
+// NewComparator
+func NewComparator(options *Options) Comparator {
+	return &comparator{
+		options.Predefined,
+		options.ImageCache,
+		options,
+	}
+}
+
+// NewComparatorWithCache
+func NewComparatorWithCache(options *Options, c *cache.Cache) Comparator {
+	return &comparator{
+		predefined: options.Predefined,
+		cache:      c,
+		options:    options,
+	}
+}
