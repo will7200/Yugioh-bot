0055cf555508ec1f39bd5399b36ab1a4552e2855
William Flores <will7207@gmail.com>
2018-05-29 14:33:50 -0700

    refactor(bot): remote unneeded function signatures from Bot interface
    These methods were originally in python port, will be implemented in lua scripts

--- bot/dl/bot.go
+++ bot/dl/bot.go
@@ -1,11 +1,7 @@
 package dl
 
 type Bot interface {
-	BattleMode(battle, version, info string)
-	GuidedMode()
-	PossibleBattlePoints()
 	SpecialEvents(info DuelLinksInstanceInfo)
-	SystemCall()
 	ImgToString()
 }
 
@@ -13,23 +9,7 @@ type BaseBot struct {
 	provider Provider
 }
 
-func (*BaseBot) BattleMode(battle, version, info string) {
-	log.Panic("implement me")
-}
-
-func (*BaseBot) GuidedMode() {
-	log.Panic("implement me")
-}
-
-func (*BaseBot) PossibleBattlePoints() {
-	log.Panic("implement me")
-}
-
 func (*BaseBot) SpecialEvents(info DuelLinksInstanceInfo) {
-	log.Panic("implement me")
-}
-
-func (*BaseBot) SystemCall() {
 	log.Panic("implement me")
 }
 
